fe3bf9a00fbc27d93a0a2a18d57b7b9c97c851ed
Shawn Strasser <shawn.strasser@odot.state.or.us>
2022-06-20 20:01:55 -0700

    update

--- Daily_Downloader.py
+++ Daily_Downloader.py
@@ -1,6 +1,11 @@
 # This script is to be run each morning, to update signal controller data
 
-from GetData import GetData
-GetData().update_data('Actuations')
-GetData().update_data('Communications')
-GetData().update_data('MaxOuts')
+import DataProcessing as dp
+# Get aggregate data tables from SQL Server
+dp.Get_SQL_Data().update_data('Actuations')
+dp.Get_SQL_Data().update_data('Communications')
+dp.Get_SQL_Data().update_data('MaxOuts')
+dp.Get_SQL_Data(server='SP2SQLMAX101', database='MaxView_EventLog').update_data('MaxTimeFaults')
+
+# Get Travel Time data from RITIS
+dp.Get_RITIS_Data().update_data()
